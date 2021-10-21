7f188bf4951164ee4eabd4b22c40fea47825d54a
Yi Joon Choi <choi990406@gmail.com>
2021-10-22 03:27:55 +0900

    [#5] BaseVC Then 사용해서 리팩토링

--- 29th_Youtube/29th_Youtube/Configuration/BaseVC.swift
+++ 29th_Youtube/29th_Youtube/Configuration/BaseVC.swift
@@ -6,43 +6,47 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class BaseVC: UIViewController {
     
-    var googleImg = UIImageView()
     var mainLabel = UILabel()
     var infoLabel = UILabel()
-    
-    var nameTF = UITextField()
-    var emailTF = UITextField()
-    var pwTF = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setNavigation()
-        
-        view.addSubviews([googleImg, mainLabel, infoLabel, nameTF, emailTF, pwTF])
-        
-        googleImgLayout()
-        nameTFLayout()
-        emailTFLayout()
-        pwTFLayout()
+        setupAutoLayout()
     }
 
     private func setNavigation(){
         self.navigationController?.navigationBar.isHidden = true
     }
     
-    private func googleImgLayout(){
-        googleImg.image = UIImage(named: "googleImg")
-        googleImg.contentMode = .scaleAspectFit
-        googleImg.snp.makeConstraints{ (make) in
-            make.width.equalTo(160)
-            make.height.equalTo(70)
-            make.centerX.equalTo(self.view)
-            make.top.equalTo(70)
-        }
+    let googleImg = UIImageView().then{
+        $0.image = UIImage(named: "googleImg")
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    let nameTF = UITextField().then{
+        $0.placeholder = "이름을 입력해주세요"
+        $0.borderStyle = .roundedRect
+        $0.autocorrectionType = .no
+    }
+    
+    let emailTF = UITextField().then{
+        $0.placeholder = "이메일 또는 휴대전화"
+        $0.borderStyle = .roundedRect
+        $0.autocorrectionType = .no
+    }
+    
+    let pwTF = UITextField().then{
+        $0.placeholder = "비밀번호 입력"
+        $0.borderStyle = .roundedRect
+        $0.isSecureTextEntry = true
+        $0.autocorrectionType = .no
     }
     
     func mainLabelLayout(labelName: String){
@@ -50,10 +54,6 @@ class BaseVC: UIViewController {
         mainLabel.textColor = .black
         mainLabel.font = UIFont.boldSystemFont(ofSize: 27.0)
         mainLabel.sizeToFit()
-        mainLabel.snp.makeConstraints{ (make) in
-            make.centerX.equalTo(self.view)
-            make.top.equalTo(googleImg.snp.bottom).offset(10)
-        }
     }
     
     func infoLabelLayout(labelName: String){
@@ -64,51 +64,54 @@ class BaseVC: UIViewController {
         infoLabel.lineBreakMode = .byWordWrapping
         infoLabel.numberOfLines = 2
         infoLabel.textAlignment = .center
-        infoLabel.snp.makeConstraints{ (make) in
-            make.centerX.equalTo(self.view)
-            make.top.equalTo(mainLabel.snp.bottom).offset(35)
-            make.trailing.equalTo(-10)
-            make.leading.equalTo(10)
-        }
     }
     
-    func nameTFLayout(){
-        nameTF.placeholder = "이름을 입력해주세요"
-        nameTF.borderStyle = .roundedRect
-        nameTF.autocorrectionType = .no
+    private func setupAutoLayout(){
+        view.addSubviews([googleImg, mainLabel, infoLabel, nameTF, emailTF, pwTF])
+        
+        googleImg.snp.makeConstraints{ (make) in
+            make.width.equalTo(160)
+            make.height.equalTo(70)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(70)
+        }
+        
+        mainLabel.snp.makeConstraints{ (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(googleImg.snp.bottom).offset(10)
+        }
+        
+        infoLabel.snp.makeConstraints{ (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(mainLabel.snp.bottom).offset(35)
+            make.leading.equalTo(10)
+            make.trailing.equalTo(-10)
+        }
+        
         nameTF.snp.makeConstraints{ (make) in
-            make.centerX.equalTo(self.view)
+            make.centerX.equalToSuperview()
             make.height.equalTo(50)
             make.top.equalTo(infoLabel.snp.bottom).offset(35)
-            make.trailing.equalTo(-30)
             make.leading.equalTo(30)
+            make.trailing.equalTo(-30)
         }
-    }
-    
-    func emailTFLayout(){
-        emailTF.placeholder = "이메일 또는 휴대전화"
-        emailTF.borderStyle = .roundedRect
-        emailTF.autocorrectionType = .no
+        
         emailTF.snp.makeConstraints{ (make) in
-            make.centerX.equalTo(self.view)
+            make.centerX.equalToSuperview()
             make.height.equalTo(50)
             make.top.equalTo(nameTF.snp.bottom).offset(15)
-            make.trailing.equalTo(-30)
             make.leading.equalTo(30)
+            make.trailing.equalTo(-30)
+        }
+        
+        pwTF.snp.makeConstraints{ (make) in
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+            make.top.equalTo(emailTF.snp.bottom).offset(15)
+            make.leading.equalTo(30)
+            make.trailing.equalTo(-30)
         }
     }
     
-    func pwTFLayout(){
-        pwTF.placeholder = "비밀번호 입력"
-        pwTF.borderStyle = .roundedRect
-        pwTF.isSecureTextEntry = true
-        pwTF.autocorrectionType = .no
-        pwTF.snp.makeConstraints{ (make) in
-            make.centerX.equalTo(self.view)
-            make.height.equalTo(50)
-            make.top.equalTo(emailTF.snp.bottom).offset(15)
-            make.trailing.equalTo(-30)
-            make.leading.equalTo(30)
-        }
-    }
+    
 }
