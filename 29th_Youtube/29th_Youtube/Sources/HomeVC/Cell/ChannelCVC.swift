714fa385ef4fbceedd588c7cdb5fa84677ec5a59
Yi Joon Choi <choi990406@gmail.com>
2021-10-29 23:37:12 +0900

    [#7] asset추가, ChannelCVC 생성

---
+++ 29th_Youtube/29th_Youtube/Sources/HomeVC/Cell/ChannelCVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelCVC.swift
+//  29th_Youtube
+//
+//  Created by Yi Joon Choi on 2021/10/29.
+//
+
+import UIKit
+
+class ChannelCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
